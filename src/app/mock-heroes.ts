e6c9dea83321b7b9618360c3d365af292dead8f3
Jonathan Vélez <mrvelez13@gmail.com>
2018-04-14 20:35:56 -0500

    Message commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,17 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Acuaman' },
+  { id: 13, name: 'Narco' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Bombasto' },
+  { id: 16, name: 'Superman' },
+  { id: 17, name: 'Green Lighning' },
+  { id: 18, name: 'Ironman' },
+  { id: 19, name: 'Viuda Negra' },
+  { id: 20, name: 'Dr IQ' },
+  { id: 21, name: 'Wolvering' },
+  { id: 22, name: 'Magneto' },
+  { id: 23, name: 'Tornado' }
+];
